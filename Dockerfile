Catskill909/icecast-deploy

commit 0fd0c98da112783fa0febee632f89641b04b467a

fix: Use /etc/supervisord.conf consistently (fixes Icecast not starting)
- Changed COPY path from /etc/supervisor/conf.d/streamdock.conf to /etc/supervisord.conf
- Updated CMD to point to /etc/supervisord.conf
- Fixes ECONNREFUSED errors by ensuring supervisor loads the config
- Critical fix: Icecast, Node.js, and Liquidsoap were not starting

Dockerfile
@@ -55,7 +55,7 @@ RUN chmod 666 /etc/icecast2/icecast.xml
 RUN chown -R liquidsoap:liquidsoap /var/log/icecast2
 
 # Supervisor config
-COPY supervisord.conf /etc/supervisor/conf.d/streamdock.conf
+COPY supervisord.conf /etc/supervisord.conf
 
 # Liquidsoap config (template only - regenerated on startup with webhooks)
 COPY radio.liq /app/radio.liq.template
@@ -80,4 +80,4 @@ EXPOSE 8001
 # Start supervisor
 # Override the Liquidsoap image's ENTRYPOINT so our CMD runs directly
 ENTRYPOINT []
-CMD ["supervisord", "-n", "-c", "/etc/supervisor/supervisord.conf"]
+CMD ["supervisord", "-n", "-c", "/etc/supervisord.conf"]

commit 65cca1e9d6739590ef40e9941905116505903c20

fix: REVERT to working supervisor config paths
Reverts broken changes from commit 0fd0c98.

The savonet/liquidsoap image uses Debian supervisor layout.
Restoring working config from b247ea7 (Dec 26).

Dockerfile
@@ -55,7 +55,7 @@ RUN chmod 666 /etc/icecast2/icecast.xml
 RUN chown -R liquidsoap:liquidsoap /var/log/icecast2
 
 # Supervisor config
-COPY supervisord.conf /etc/supervisord.conf
+COPY supervisord.conf /etc/supervisor/conf.d/streamdock.conf
 
 # Liquidsoap config (template only - regenerated on startup with webhooks)
 COPY radio.liq /app/radio.liq.template
@@ -80,4 +80,4 @@ EXPOSE 8001
 # Start supervisor
 # Override the Liquidsoap image's ENTRYPOINT so our CMD runs directly
 ENTRYPOINT []
-CMD ["supervisord", "-n", "-c", "/etc/supervisord.conf"]
+CMD ["supervisord", "-n", "-c", "/etc/supervisor/supervisord.conf"]